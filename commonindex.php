795f7ef79b348a8985a3e1cd886d8ade39843ac0
Prathusha Pandipati <33255489+prathushapandipati@users.noreply.github.com>
2018-05-30 16:56:41 +0200

    update

---
+++ commonindex.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <title>Sport och Idrott</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS: You can use this stylesheet to override any Bootstrap styles and/or apply your own styles -->
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom Fonts from Google -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav id="siteNav" class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container" background-color ="white" >
+            <!-- Logo and responsive toggle -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">
+                	<span class="glyphicon glyphicon-fire"></span> 
+                	Sport och Idrott
+                </a>
+            </div>
+                  
+        </div><!-- /.container -->
+    </nav>
+
+	<!-- Header -->
+    <header>	
+        <div class="header-content">
+		
+		     <div class="header-content-inner">			  
+                <h1>Skapa Konto här</h1>            
+               
+		
+		
+		          <?php require_once("User_Registration.php"); ?>
+		
+	
+			 
+            </div>
+        </div>
+    </header>	
+
+	<!-- Content 3 -->
+     <section class="content content-3">
+        <div class="container">
+			<h2 class="section-header"> About</h2>
+			<p class="lead text-muted">Holisticly predominate extensible testing procedures for reliable supply chains. Dynamically innovate resource-leveling customer service for state of the art customer service. jfhfgygesjgh bgjrgjgf vdfjgvfjd fbjvfgdjbgfd djhdjbds sdhbjdbgdjs hsufjsafbs sjhdfgjsdfbsj</p> 
+                  <!--  <a href="#" class="btn btn-primary btn-lg">Check Now</a>               -->
+            </div>
+        </div>
+    </section>
+    
+	
+	
+	<!-- Footer -->
+    <footer class="page-footer">
+    
+    	<!-- Contact Us -->
+        <div class="contact">
+        	<div class="container">
+				<h2 class="section-heading">Contact Us</h2>
+				<p><span class="glyphicon glyphicon-earphone"></span><br> +46 734 89 47 62</p>
+				<p><span class="glyphicon glyphicon-envelope"></span><br> info@sportochidrott.com</p>
+        	</div>
+        </div>
+        	
+        <!-- Copyright etc -->
+        <div class="small-print">
+        	<div class="container">
+        		<p>Copyright &copy; www.sportochidrott.com 2018</p>
+        	</div>
+        </div>
+        
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery-1.11.3.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+    
+    <!-- Custom Javascript -->
+    <script src="js/custom.js"></script>
+
+</body>
+
+</html>
